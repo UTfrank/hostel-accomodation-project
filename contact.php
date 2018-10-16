78fb6b49dee56a7010a994eee05a96ebe300b08e
franklin_john <frankjoe261@gmail.com>
2018-10-16 17:01:27 +0100

    the full app

---
+++ contact.php
@@ -0,0 +1,9 @@
+<h1 style="color:#000000"><b><center>Contact Details</center></b></h1>
+<div align="center">
+<p><b>Address :</b> Riivers State University of Science and Technology.
+<p><b>Phone :</b> 08030000000<br>
+<b>Mobile :</b> +234 817 123 4567
+</p>
+<p><b>Email :</b> johndoe@mail.com</p>
+</p>
+</div>
